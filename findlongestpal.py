8a145a49e7ef2f20823700c3aa0083c2b41ce8c8
Brian Baron <brianbaron@gmail.com>
2018-02-09 01:30:27 -0800

    completed findlongestpal.py palandrom challenge

---
+++ findlongestpal.py
@@ -0,0 +1,77 @@
+def FindLongestPalRecursive(a):
+    '''This is a dumb recursive function to find a palindrom.  It doesn't really
+    work since it doesn't take into account unbalanced strings like:
+    "XSDGREWEFmom"
+    '''
+    a = a.replace(" ","")
+    if len(a) <= 1:
+        a = False
+        return a
+    if a == a[::-1]:
+        return a
+    a = a[1:]
+    if a == a[::-1]:
+        return a
+    a = a[:-1]
+    if a == a[::-1]:
+        return a
+    return FindLongestPal(a)
+
+
+def FindLongestPal(a):
+    '''Same as above, but not recursive'''
+    a = a.replace(" ","")
+    while len(a) >= 1:
+        if a == a[::-1]:
+            return a
+        a = a[1:]
+        if a == a[::-1]:
+            return a
+        a = a[:-1]
+        if a == a[::-1]:
+            return a
+    return False
+
+
+def FindLongestPalReal(a):
+"""This one actually works"""
+    a = a.replace(" ", "")
+    list_of_pal = []
+    if a == a[::-1]:
+        return a
+    if len(a) <= 1:
+        return a
+    ix = 0
+    #check2
+    while ix < len(a) - 1:
+        if a[ix] == a[ix+1]:
+            list_of_pal.append([ix,ix + 1])
+        if ix + 2 <= len(a)-1:
+            if a[ix:ix+2] == a[ix+2:ix:-1]:
+                list_of_pal.append([ix, ix + 1, ix + 2])
+        ix += 1
+
+    ix = 0
+    result = []
+    for starts in list_of_pal:
+        is_pal = True
+        front = starts[0]
+        end = starts[-1]
+        while is_pal:
+            result.append([len(starts), sorted(starts)])
+            if front - 1 >= 0 and end + 1 <= len(a) - 1:
+                if a[front - 1] == a[end + 1]:
+                    starts.extend([front - 1, end + 1])
+                    front -= 1
+                    end += 1
+                else: is_pal = False
+            else: is_pal = False
+            result = sorted(result)
+    return a[result[-1][1][0]:result[-1][1][-1]+1]
+
+
+
+print(FindLongestPalReal("cattaccattacblskddidmmooommm"))
+# print(FindLongestPalRecursive("ac man a plan a canal panama"))
+#
+# print(FindLongestPal("ac man a plan a canal panama"))
