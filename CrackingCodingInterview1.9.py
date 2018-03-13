caca462fa9e1c8b3c999b2ac77bc2c00faa395f1
Brian Baron <brianbaron@gmail.com>
2018-03-13 21:32:47 +0800

    docs: add finished CrackingCodingInterview1.9.py

--- CrackingCodingInterview1.9.py
+++ CrackingCodingInterview1.9.py
@@ -1,12 +1,28 @@
-
-def is_rotation(s1,s2):
+def is_rotation(s1, s2):
+''' String Rotation: Assume you have a method iSubstring which checks is one
+    word is a substring of another. Given two strings, s1 and s2, write code to
+    check if s2 is a rotation of s1 using only one call to isSubstring.'''
+    if not len(s1) == len(s2):
+        return False
     i = 1
-    s2temp = ""
-    s1temp = ""
-    while not s2temp == s1temp:
-        s1temp = s1[i:-i]
-        s2temp = s2[i:-i]
-        print s1temp
+    while i < len(s1):
+        if s1[i:] == s2[0:-i] and s1[:i] == s2[-i:]:
+            return True
         i += 1
+    return False
 
-is_rotation("waterbottle", "erbottlewat")
+
+# def is_rotation2(s1, s2):
+#     ''' String Rotation: Assume you have a method isSubstring which checks if one
+#     word is a substring of another. Given two strings, s1 and s2, write code to
+#     check if s2 is a rotation of s1 using only one call to isSubstring.'''
+#     if (not len(s1) == len(s2)) or len(s1) < 1:
+#         return False
+#     s1s1 = s1 + s1
+#     return isSubstring(s1s1, s2)
+
+
+print(is_rotation("waterbottle", "bottle"))
+print(is_rotation("waterbottle", "erbottlewat"))
+print(is_rotation("animal", "lanima"))
+print(is_rotation("aaaabottle", "bottleeeea"))
